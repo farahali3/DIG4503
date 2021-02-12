b16d132791cb17b2930a54b175060fe73088f77c
farahali3 <72891595+farahali3@users.noreply.github.com>
2021-02-11 23:43:44 -0500

    lab4

---
+++ lab4/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Market from './Components/market'
+
+function App() {
+  return (
+<Market />
+  );
+}
+
+export default App;
---
+++ lab4/src/Components/item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function marketItem (props) {
+    return (
+        <div>
+<p>Item {props.count} </p>
+        </div>
+    );
+}
+
+export default marketItem; 
