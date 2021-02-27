9d648dae0962a92ff92944614f84e5e814a90df1
farahali3 <72891595+farahali3@users.noreply.github.com>
2021-02-26 21:52:10 -0500

    lab6

---
+++ lab6/index.js
@@ -0,0 +1,15 @@
+const Express = require("express");
+const App = Express();
+const port = 3003;
+
+
+App.get('/', function(req, res){
+    res.send("Hello World!");
+});
+
+App.use('/public', Express.static("public"));
+
+App.listen(port, function (){
+    console.log("Server Running!");
+}
+)
