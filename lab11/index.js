e240ef2b920feae88d11aa7d07f405e453a872c0
farahali3 <72891595+farahali3@users.noreply.github.com>
2021-04-09 19:08:11 -0400

    lab11

--- lab11/index.js
+++ lab11/index.js
@@ -7,7 +7,7 @@ MongoClient.connect(URL, { useUnifiedTopology: true})
     let database = connection.db("sample_airbnb");
     let collection = database.collection("listingsAndReviews");
 
-    let cursor = collection.find({review_scores: {$gte: "99"}, beds: {$gte:5}, price: {$lte:200}});
+    let cursor = collection.find({review_scores: {$gte: 99}, beds: {$gte:5}, price: {$lte:200}});
 
     cursor.forEach(document => {
         console.log(document.name);
