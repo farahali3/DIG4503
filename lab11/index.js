0d15279b8ab5e39e586a272a66da49835d01f325
farahali3 <72891595+farahali3@users.noreply.github.com>
2021-04-09 19:01:27 -0400

    lab11

---
+++ lab11/index.js
@@ -0,0 +1,20 @@
+import MongoClient from 'mongodb';
+
+const URL = "mongodb+srv://fali:zYBrwPVUqaoUumlk@cluster0.oewwc.mongodb.net/";
+
+MongoClient.connect(URL, { useUnifiedTopology: true})
+.then(connection => {
+    let database = connection.db("sample_airbnb");
+    let collection = database.collection("listingsAndReviews");
+
+    let cursor = collection.find({review_scores: {$gte: "99"}, beds: {$gte:5}, price: {$lte:200}});
+
+    cursor.forEach(document => {
+        console.log(document.name);
+    }, () => {
+        connection.close();
+    })
+})
+.catch(error => {
+    console.log("Error: " + error);
+})
